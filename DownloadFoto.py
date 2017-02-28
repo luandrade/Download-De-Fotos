1231b7636370353dd9766a351c61a4605f1baf88
Lucas Andrade Rodrigues <lucas.andrade.rodrigues.13@gmail.com>
2017-02-27 21:48:01 -0300

    Delete DownloadFoto.py

--- DownloadFoto.py
+++
@@ -1,8 +0,0 @@
-#este programa faz o dowload de fotos
-import urllib.request
-#ele irá ler o arquivo que está em binário
-foto = urllib.request.urlopen('http://www.bestriders.com.br/newblog/wp-content/uploads/2016/01/TESTE_KAWASAKI_NINJA_H2R_06.jpg').read()
-#e ira transforma-lo em um arquivo, sendo assim virará uma imágem  
-file = open('moto1.png', 'wb')
-file.write(foto)
-file.close()
